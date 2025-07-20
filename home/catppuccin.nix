4f96e599969ba5441f4bdd3246eb6f008627bdb8
Ricardo Trindade <ricardopovoas.trindade@gmail.com>
2025-07-20 23:08:28 +0100

    add icon themes for gtk theme

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -7,19 +7,35 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Mocha";
+      name = "Catppuccin-Mocha-Compact-Blue-Dark";
       package = pkgs.catppuccin-gtk.override {
         accents = [ "blue" ];
         size = "compact";
         variant = "mocha";
       };
     };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 
+  home.sessionVariables.GTK_THEME = "Catppuccin-Mocha-Compact-Blue-Dark";
+  
   # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
   xdg.configFile = {
     "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
     "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
     "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-};
+  };
 }
